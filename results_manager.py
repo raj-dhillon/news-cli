c61479b127bccde816b26b3896b8404d6bcecafe
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2022-11-15 22:07:39 -0800

    fixed relative paths from terminal aliasing

--- results_manager.py
+++ results_manager.py
@@ -1,17 +1,20 @@
 import json
 import os
+from pathlib import Path
 
 
 class ResultsManager:
 
     def __init__(self, results=None):
         self.results = results
+        self.path = Path(__file__).parent.joinpath("results_storage.json")
 
     def export_results(self):
-        with open("./results_storage.json", "w") as output_file:
+
+        with open(self.path, "w") as output_file:
             json.dump(self.results, output_file)
 
     def import_results(self):
-        if os.path.isfile("./results_storage.json"):
-            with open("./results_storage.json", "r") as input_file:
+        if os.path.isfile(self.path):
+            with open(self.path, "r") as input_file:
                 self.results = json.load(input_file)
